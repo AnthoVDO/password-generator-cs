a83b6738922c892961a5af7bb6b815bdbd9e702a
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-10-05 21:20:00 +0200

    I did a password generator to practice c#

---
+++ password_generator/Program.cs
@@ -0,0 +1,77 @@
+﻿using FormationCS;
+using System;
+
+namespace password_generator
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            const int NUMBER_OF_PASSWORD = 10;
+            //1 - ask password length by creating a module that we can use after because it's a function that we ask a lot
+            int passWordLength = outils.AskPositiveNumber("What is the password lenght ? ");
+
+            // You would like a password with :
+            // 1 - Only minus
+            // 2 - Only minus and capital
+            // 3 - Only alphabet and number
+            // 4 - alphabet, number and special character
+            int passwordType = outils.AskNumberBetweenMinAndMax("You would like a password with: \n" +
+                "1 - Only minus \n" +
+                "2 - Only minus and capital \n" +
+                "3 - Only alphabet and number \n" +
+                "4 - Alphabet, number and special character\n" +
+                "Your choice: ", 1, 4);
+            Console.WriteLine("Choose your password: ");
+            Console.WriteLine("");
+
+            string minus = "abcdefghijklmnopqrstuvwxyz";
+            string capital = minus.ToUpper();
+            string number = "0123456789";
+            string specialCharacter = "#$_-&+*/";
+            string alphabet;
+
+            switch (passwordType)
+            {
+                case 1:
+                    alphabet = minus;
+                    break;
+                case 2:
+                    alphabet = minus + capital;
+                    break;
+                case 3:
+                    alphabet = minus + capital + number;
+                    break;
+                default:
+                    alphabet = minus + capital + number + specialCharacter;
+                    break;
+            }
+            //2 - create an alphabet
+            //3 - choose a random character
+            
+            for(int j=0; j<NUMBER_OF_PASSWORD; j++)
+            {
+            string password = "";
+            int l = alphabet.Length;
+            Random rand = new Random();
+
+            for(int i=0; i<passWordLength; i++)
+            {
+                int index = rand.Next(0, l);
+                password += alphabet[index];
+            }
+
+
+            
+            Console.WriteLine(password);
+            }
+            
+
+
+
+            //4 - generate the password  
+            //5 - FEATURE : choose the kind a password needed
+        }
+    }
+}
---
+++ password_generator/outils.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class outils
+    {
+        public static int AskPositiveNumber(string question)
+        {
+            int number = AskNumber(question);
+            if (number > 0)
+            {
+                return number;
+            }
+            else
+            {
+                Console.WriteLine("Error: The number should be above zero");
+                return AskNumber(question);
+            }
+        }
+        public static int AskNumberBetweenMinAndMax(string question, int min, int max)
+        {
+            int number = min - 1;
+            while ((number < min) || (number > max))
+            {
+                number = AskNumber(question);
+                if ((number < min) || (number > max))
+                {
+                    Console.WriteLine($"The value should be between {min} and {max}");
+                }
+
+            }
+            return number;
+        }
+        public static int AskNumber(string question)
+        {
+            // ask question
+
+            // take the answer
+
+            // convert
+            // handle error
+            // loop until valid answer
+
+            while (true)
+            {
+                Console.Write(question + ": ");
+                string answer = Console.ReadLine();
+                int number = 0;
+                try
+                {
+                    number = int.Parse(answer);
+                    return number;
+                }
+                catch
+                {
+                    Console.WriteLine("Error: you need to enter a valid number");
+                }
+            }
+
+        }
+    }
+}
